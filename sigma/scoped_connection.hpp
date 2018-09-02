09bbfaa6720eb4b6d67ac1dc32e9c3ab213c9e18
Frederik Engels <frederik.engels24@gmail.com>
2018-09-02 21:05:22 +0200

    scoped_connection for automatic disconnections

---
+++ sigma/scoped_connection.hpp
@@ -0,0 +1,32 @@
+#ifndef SIGMA_SCOPED_CONNECTION_HPP_INCLUDED
+#define SIGMA_SCOPED_CONNECTION_HPP_INCLUDED
+
+#pragma once
+
+namespace sigma
+{
+class scoped_connection {
+private:
+    connection m_con;
+
+public:
+    /**
+     * move to indicate we are transferring ownership to this object
+     */
+    scoped_connection(connection&& c) : m_con{std::move(c)}
+    {}
+
+    scoped_connection(const scoped_connection&) noexcept = delete;
+    scoped_connection& operator=(const scoped_connection&) noexcept = delete;
+
+    scoped_connection(scoped_connection&&) noexcept = default;
+    scoped_connection& operator=(scoped_connection&&) noexcept = default;
+
+    ~scoped_connection()
+    {
+        m_con.disconnect();
+    }
+};
+} // namespace sigma
+
+#endif
