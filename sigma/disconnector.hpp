d58d169f40eba045f4be7165d55d2a334317c6c7
Frederik Engels <frederik.engels24@gmail.com>
2018-09-02 21:05:07 +0200

    base for managing disconnecting

---
+++ sigma/disconnector.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <memory>
+
+namespace sigma
+{
+class disconnector_base : std::enable_shared_from_this<disconnector_base> {
+protected:
+    disconnector_base() = default;
+
+public:
+    std::weak_ptr<disconnector_base> track() noexcept
+    {
+        return weak_from_this();
+    }
+
+    virtual void operator()(connection& c) = 0;
+};
+} // namespace sigma
